497ef8004883924116d74e8eea146b903fc15b0f
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-18 11:49:38 -0300

    :lipstick: chore: define colors pallete

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <div>
       <h1>Financeiro</h1>
+      <p className="text-danger">Lorem ipsum</p>
+      <p className="text-warning">Lorem ipsum</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#38a169",
+          100: "#f0fff4",
+          200: "#c6f6d5",
+          300: "#9ae6b4",
+          400: "#68d391",
+          500: "#48bb78",
+          600: "#38a169",
+          700: "#2f855a",
+          800: "#276749",
+          900: "#22543d",
+        },
+        danger: "#e53e3e",
+        warning: "#ed8936",
+        "background-dark": "#0b1613"
+      },
+    },
   },
   plugins: [],
 }
