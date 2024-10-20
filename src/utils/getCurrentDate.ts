31d687576293a549e73a4f2ba2aecb9e815573a7
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-20 13:36:17 -0300

    :sparkles: feat: add functions to calculate month changes

--- src/utils/getCurrentDate.ts
+++ src/utils/getCurrentDate.ts
@@ -1,4 +1,6 @@
-export const getCurrentDay = () => {
+import months from "../constaints/months"
+
+export const getCurrentDay = (): number => {
   const date = new Date()
   return date.getDate()
 }
@@ -6,4 +8,49 @@ export const getCurrentDay = () => {
 export const getCurrentMonthDate = (): string => {
   const date = new Date()
   return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}`
+}
+
+export const getMonthLabel = (monthDate: string): string => {
+  const [yearString, monthString] = monthDate.split("-")
+  const month = parseInt(monthString)
+  const monthName = months[month - 1]
+  
+  if(monthName) {
+    return `${monthName.toUpperCase()} ${yearString}`
+  }
+
+  return ""
+}
+
+export const getMonthYearNumber = (monthDate: string): { month: number, year: number } | null => {
+  const [yearString, monthString] = monthDate.split("-")
+
+  const month = parseInt(monthString)
+  const year = parseInt(yearString)
+
+  if(isNaN(month) || month > 11 || month < 0 || isNaN(year)) {
+    return null
+  }
+
+  return { month, year }
+}
+
+export const changeMonthDate = (monthDate: string, position: number): string => {
+  const [yearString, monthString] = monthDate.split("-")
+  let month = parseInt(monthString)
+  let year = parseInt(yearString)
+
+  month += position
+  
+  while (month < 1) {
+    month += 12;
+    year--
+  }
+
+  while (month > 12) {
+    month -= 12;
+    year++
+  }
+
+  return `${year}-${String(month).padStart(2, "0")}`
 }
