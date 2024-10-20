cf59b0dbded70df9af8c57680bf18d49fbb98594
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-20 12:17:54 -0300

    :sparkles: feat: add new boxes in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,25 @@
 import Header from "../components/ui/Header"
 import SideBar from "../components/SideBar"
+import BalancesBox from "../components/dashboard/BalancesBox"
+import ExpensesBox from "../components/dashboard/ExpensesBox"
+import IncomesBox from "../components/dashboard/IncomesBox"
 
 const DashboardPage = () => {
   return (
-    <div className="flex">
+    <div className="flex h-full">
       <aside>
         <SideBar />
       </aside>
 
-      <Header showMenuButton />
+      <div className="flex flex-col flex-1 h-full">
+        <Header showMenuButton />
 
-      <main className="flex-1 flex items-center pt-8">
-      </main>
+        <main className="w-full h-full flex-col gap-8 flex p-8 shadow-inner overflow-auto">
+          <BalancesBox />
+          <ExpensesBox />
+          <IncomesBox />
+        </main>
+      </div>
     </div>
   )
 }
