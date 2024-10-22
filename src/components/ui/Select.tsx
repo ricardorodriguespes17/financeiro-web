594e71d666718e7d2a1f35a796c1cf8e17638044
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-22 15:31:25 -0300

    :sparkles: feat: create select component

---
+++ src/components/ui/Select.tsx
@@ -0,0 +1,40 @@
+import { SelectHTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge"
+
+type SelectProps = {
+  label?: string
+  error?: string
+  options: OptionProps[]
+} & SelectHTMLAttributes<HTMLSelectElement>
+
+type OptionProps = {
+  label: string
+  value: string
+}
+
+const Select = ({ label, error, options, ...rest }: SelectProps) => {
+  const className = twMerge(
+    "w-full h-10 px-4",
+    "border border-gray-300 rounded-md focus:border-primary",
+    "drop-shadow-sm focus:drop-shadow-md"
+  )
+
+  return (
+    <div className="w-full flex flex-col gap-1">
+      <label>{label}</label>
+      <select
+        className={className}
+        {...rest}
+      >
+        {options.map(item => (
+          <option key={item.value} value={item.value}>
+            {item.label}
+            </option>
+        ))}
+      </select>
+      <label className="text-danger text-sm">{error}</label>
+    </div>
+  )
+}
+
+export default Select
