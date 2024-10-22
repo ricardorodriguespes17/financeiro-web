cd983fe86d3647346b077d36c16970ec819040dc
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-22 10:10:52 -0300

    :recycle: refactor: transference controller returns

--- src/controller/transferenceController.ts
+++ src/controller/transferenceController.ts
@@ -1,3 +1,4 @@
+import { ControllerResponseType } from "../@types/ControllerResponseType"
 import { TransferenceCreateType, TransferenceType } from "../@types/TransferenceType"
 import ApiService from "../services/ApiService"
 
@@ -6,16 +7,65 @@ class TransferenceController {
     return await ApiService.get<TransferenceType[]>(`/transferences/${boardId}`)
   }
 
-  async createTransference(data: TransferenceCreateType) {
-    return await ApiService.post("/transferences", data)
+  async createTransference(data: TransferenceCreateType): Promise<ControllerResponseType> {
+    try {
+      const response = await ApiService.post("/transferences", data)
+
+      return {
+        title: "Sucesso",
+        content: response.data.message,
+        type: "success"
+      }
+    } catch (err) {
+      const error = err as { response: { data: { message: string } } }
+
+      return {
+        title: "Erro ao criar a transferência",
+        content: error.response.data.message,
+        type: "error"
+      }
+    }
   }
 
-  async updateTransference(transferenceId: string, data: TransferenceCreateType) {
-    return await ApiService.post(`/transferences/${transferenceId}`, data)
+  async updateTransference(transferenceId: string, data: TransferenceCreateType): Promise<ControllerResponseType> {
+    try {
+      const response = await ApiService.post(`/transferences/${transferenceId}`, data)
+
+      return {
+        title: "Sucesso",
+        content: response.data.message,
+        type: "success"
+      }
+    } catch (err) {
+      const error = err as { response: { data: { message: string } } }
+
+      return {
+        title: "Erro ao atualizar a tranferência",
+        content: error.response.data.message,
+        type: "error"
+      }
+    }
   }
 
-  async deleteTransference(transferenceId: string) {
-    return await ApiService.post(`/transferences/${transferenceId}`)
+  async deleteTransference(transferenceId: string): Promise<ControllerResponseType> {
+
+    try {
+      const response = await ApiService.post(`/transferences/${transferenceId}`)
+
+      return {
+        title: "Sucesso",
+        content: response.data.message,
+        type: "success"
+      }
+    } catch (err) {
+      const error = err as { response: { data: { message: string } } }
+
+      return {
+        title: "Erro ao deletar a tranferência",
+        content: error.response.data.message,
+        type: "error"
+      }
+    }
   }
 }
 
