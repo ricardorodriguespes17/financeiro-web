0ee94e72fb4cacdc5ca88efa038336f50323385d
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-28 23:56:56 -0300

    :recycle: refactor: side bar interactive menu

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,28 +6,31 @@ import useMenuStore from "../store/menuStore"
 import useTheme from "../store/themeStore"
 import { FaMoon, FaRegSun, FaUser } from "react-icons/fa"
 import useAuthActions from "../hooks/useAuthActions"
+import { useState } from "react"
+import { useLocation } from "react-router-dom"
 
 const SideBar = () => {
   const { isOpened } = useMenuStore()
   const { logout } = useAuthActions()
   const { toggleTheme, themeMode } = useTheme()
+  const { pathname } = useLocation()
 
   const handleLogout = async () => {
     await logout()
   }
 
-  const menu = [
-    { label: "Dashboard", href: "/dashboard", Icon: LuLayoutDashboard, selected: true },
-    { label: "Perfil", href: "/profile", Icon: FaUser },
+  const [menu] = useState([
+    { label: "Dashboard", href: "/dashboard", Icon: LuLayoutDashboard, selected: pathname === "/dashboard" },
+    { label: "Perfil", href: "/profile", Icon: FaUser, selected: pathname === "/profile" },
     "separator",
     { label: "Tema", onClick: toggleTheme, Icon: themeMode === "dark" ? FaRegSun : FaMoon },
     { label: "Sair", onClick: handleLogout, Icon: IoPower }
-  ]
+  ])
 
   const className = twMerge(
     "h-full bg-white dark:bg-black flex flex-col items-center gap-3 pt-4 overflow-hidden",
-    "transition-[width] duration-500 ease-in-out fixed md:static top-[76px]",
-    isOpened ? "w-[270px] px-4" : "md:w-[90px] w-0 px-0",
+    "transition-[width] duration-500 ease-in-out fixed md:static top-[76px] px-4",
+    isOpened ? "w-[270px]" : "md:w-[90px] w-0",
   )
 
   const titleClassName = twMerge(
@@ -40,6 +43,11 @@ const SideBar = () => {
     "overflow-hidden transition-all duration-300 text-nowrap ease-in-out"
   )
 
+  const buttonClassName = twMerge(
+    "w-full gap-0",
+    isOpened ? "justify-start pl-4" : "justify-center pl-0"
+  )
+
   return (
     <div className={className}>
       <h2 className={titleClassName}>
@@ -48,7 +56,7 @@ const SideBar = () => {
 
       {menu.map((item, index) => {
         if (typeof item === "string")
-          return <hr key={index} className="border-separate" />
+          return <hr key={index} className="border-separate border-primary/30 w-full" />
 
         return (
           <Button
@@ -57,7 +65,7 @@ const SideBar = () => {
             onClick={item.onClick}
             variant={item.selected ? "solid" : "plain"}
             size={isOpened ? "full" : "fit"}
-            className="justify-start px-4 gap-0"
+            className={buttonClassName}
           >
             <item.Icon size={24} className="min-w-6" />
             <label className={labelClassName}>
