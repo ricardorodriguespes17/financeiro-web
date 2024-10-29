a37d67070a1b6fbf7e92f23da52ebd215872c0aa
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-28 23:48:15 -0300

    :bug: fix: api service interceptor filter error

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from "axios"
 import authController from "../controller/authController"
 import useAuthStore from "../store/authStore"
+import useAuthActions from "../hooks/useAuthActions"
 
 const baseURL = import.meta.env.VITE_API_URL
 
@@ -20,51 +21,57 @@ const configInterceptor = () => {
     const originalRequest = error.config
 
     if (error.response.status === 401 && !originalRequest._retry) {
-      if (error.response.data.message) {
-        const { refreshToken } = useAuthStore.getState()
-        if (!isRefreshing && refreshToken) {
-          originalRequest._retry = true
-          isRefreshing = true
+      const { refreshToken, accessToken } = useAuthStore.getState()
 
-          try {
-            const response = await authController.refreshToken(refreshToken)
-            const accessToken = response.data.accessToken
-
-            setAuthHeader(accessToken)
-
-            failedRequestsQueue.forEach((req) => req.resolve(accessToken))
-            failedRequestsQueue = []
-
-            return await api(originalRequest)
-          } catch (refreshError) {
-            failedRequestsQueue.forEach((req) => req.reject(refreshError as AxiosError))
-            failedRequestsQueue = []
-
-            await authController.logout(refreshToken)
-          } finally {
-            isRefreshing = false
-          }
-        }
-
-        return new Promise((resolve, reject) => {
-          failedRequestsQueue.push({
-            resolve: (accessToken: string | null) => {
-              originalRequest.headers["Authorization"] = accessToken
-              resolve(api(originalRequest))
-            },
-            reject: (err: AxiosError) => {
-              reject(err)
-            },
-          })
-        })
-      } else {
-        await authController.logout(null)
+      if(error.response.data.message === "Token não enviado") {
+        setAuthHeader(accessToken)
+        return await api(originalRequest)
       }
+
+      if (!isRefreshing && refreshToken) {
+        originalRequest._retry = true
+        isRefreshing = true
+
+        try {
+          const response = await authController.refreshToken(refreshToken)
+          const accessToken = response.data.accessToken
+
+          setAuthHeader(accessToken)
+
+          failedRequestsQueue.forEach((req) => req.resolve(accessToken))
+          failedRequestsQueue = []
+
+          return await api(originalRequest)
+        } catch (refreshError) {
+          const error = refreshError as AxiosError
+
+          failedRequestsQueue.forEach((req) => req.reject(error))
+          failedRequestsQueue = []
+
+          if (error.status === 401) {
+            const { logout } = useAuthActions()
+            await logout()
+          }
+        } finally {
+          isRefreshing = false
+        }
+      }
+
+      return new Promise((resolve, reject) => {
+        failedRequestsQueue.push({
+          resolve: (accessToken: string | null) => {
+            originalRequest.headers["Authorization"] = accessToken
+            resolve(api(originalRequest))
+          },
+          reject: (err: AxiosError) => {
+            reject(err)
+          },
+        })
+      })
     }
 
     return Promise.reject(error)
-  }
-  )
+  })
 }
 
 configInterceptor()
